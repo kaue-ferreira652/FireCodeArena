306563bca904d52d8097c697f4f64cd0d0d5f5ad
kaue-ferreira652 <10kaue981@gmail.com>
2026-06-16 16:01:27 -0300

    feat:Subindo exercício 11

---
+++ ex11.js
@@ -0,0 +1,8 @@
+let numeros = [50, 10, 75, 100, 25]
+let dobro = []
+
+for(let i = 0; i < numeros.length; i++){
+    dobro.push(numeros[i] * 2)
+}
+console.log("Números originais: " + numeros)
+console.log("Dobro dos números: " + dobro)
