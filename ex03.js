63a99e3c6f8013ba55bfca32557edaa6099ee420
kaue-ferreira652 <10kaue981@gmail.com>
2026-06-16 15:55:55 -0300

    feat:Subindo exercício 3

---
+++ ex03.js
@@ -0,0 +1,9 @@
+let idades = [12, 14, 18, 25, 32]
+
+for(let i = 0; i < idades.length; i++){
+    if(idades[i] >= 18){
+        console.log(`A pessoa é maior de idade`);
+    } else {
+        console.log(`A pessoa é menor de idade`);
+    }
+}
