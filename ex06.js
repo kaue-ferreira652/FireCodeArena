1a0f0b3b487289d7a3063b93368195d550490cbf
kaue-ferreira652 <10kaue981@gmail.com>
2026-06-16 15:58:46 -0300

    feat:Subindo exercício 6

---
+++ ex06.js
@@ -0,0 +1,13 @@
+let numeros = [9, 3, 10, 4]
+let soma = 0
+for(let i = 0; i < 4; i++){
+    soma = soma + numeros[i]
+}
+console.log(soma)
+
+if(soma >= 7){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}
+
